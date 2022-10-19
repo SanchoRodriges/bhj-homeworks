29874a7e367803397a2056dc5c27187d8de72081
Sancho Rodriges <softilla@ya.ru>
2022-10-19 20:33:59 +0300

    slider n2

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,29 +3,20 @@ const sliderArrowNext = document.querySelector('.slider__arrow_next');
 
 const sliderItems = Array.from(document.querySelectorAll('.slider__item'));
 
-sliderArrowPrew.onclick = sliderPrew;
-sliderArrowNext.onclick = sliderNext;
+sliderArrowPrew.onclick = () => slide('prev');
+sliderArrowNext.onclick = () => slide('next');
 
 function slide(next) {
-    let sliderItemNext, activeSlide;
-    for (let i = 0; i < sliderItems.length; i++) {
-        if ( sliderItems[i].className.includes('slider__item_active') ) {
-            activeSlide = sliderItems[i];
-            if (next === 'next') {
-                sliderItemNext = sliderItems[i+1] !== undefined ? sliderItems[i+1] : sliderItems[0];
-            } else if (next === 'prev') {
-                sliderItemNext = sliderItems[i-1] !== undefined ? sliderItems[i-1] : sliderItems[sliderItems.length - 1];
-            }
-        }
+    let sliderItemNext, slideCount, nextSlideIndex;
+
+    let activeSlideIndex = sliderItems.findIndex( item => item.className.includes('slider__item_active') );
+
+    if (next === 'next') {
+        nextSlideIndex = activeSlideIndex < sliderItems.length - 1 ? activeSlideIndex + 1 : 0;
+    } else if (next === 'prev') {
+        nextSlideIndex = activeSlideIndex > 0 ? activeSlideIndex - 1 : sliderItems.length - 1;
     }
-    sliderItemNext.classList.add('slider__item_active');
-    activeSlide.classList.remove('slider__item_active');
+
+    sliderItems[nextSlideIndex].classList.add('slider__item_active');
+    sliderItems[activeSlideIndex].classList.remove('slider__item_active');
 }
-
-function sliderPrew() {
-    slide('prev');
-};
-
-function sliderNext() {
-    slide('next');
-};
